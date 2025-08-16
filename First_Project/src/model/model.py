95e789dc5d1341dd6f9f272a1ebb3523bc70dc60
al0214 <sisang951@gmail.com>
2025-08-16 14:52:50 +0000

    Feat

--- First_Project/src/model/model.py
+++ First_Project/src/model/model.py
@@ -1,9 +1,53 @@
 from pydantic import BaseModel
+from typing import List, Optional
+from fastapi import Form
 
 class Item(BaseModel):
     item: str
     status: str
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+    
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)
+    
+    class Config:
+        model_config = {
+            "example": {
+                "id": 1,
+                "item" : "Example Schema!"
+            }
+        }
+        
+class TodoItem(BaseModel):
+    item: str
+    
+    class Config:
+        model_config= {
+            "example":{
+                "item" : "Read the ext chapter of the book."
+            }
+        }
+        
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+    
+    class Config:
+        model_config = {
+            "example" : {
+                "todos" : [
+                    {
+                        "item" : "Example schema 1!"
+                    },
+                    {
+                        "item" : "Example schema 2!"
+                    },
+                ]
+            }
+        }
